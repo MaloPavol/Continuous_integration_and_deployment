7a3bf52b146fd212bca9d4435cb10082b82a1409
palomalo <malo.pavol@gmail.com>
2020-08-06 11:59:06 +0200

    update travis yaml

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ cd "microservice_B_(Java_wSpringBoot)"/
 mvn clean install
 mvn cobertura:cobertura
 # mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+# mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=continuous_integration_and_deployment
